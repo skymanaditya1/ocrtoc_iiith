45192d5b00ef096c89c87f6a0fdb878a0fbff373
robotics.tcs.rni <robotics.tcs.rni@gmail.com>
2022-04-22 17:58:43 +0530

    func to get 3D Vol estimate added
    following utility functions added:
    1. get_octree_size(): traverses octree to get node count
    2. get_pcd_vol(): get 3D volume estimate of given point-cloud based on voxel count

--- ocrtoc_planning/scripts/scene_utils.py
+++ ocrtoc_planning/scripts/scene_utils.py
@@ -14,6 +14,10 @@ from geometry_msgs.msg._Quaternion import Quaternion
 rospack = rospkg.RosPack()
 MESH_PARENT_PATH = os.path.join(rospack.get_path('ocrtoc_materials'), 'models')
 
+# Point-cloud
+VOXEL_SIZE = 0.01
+OCTREE_DEPTH = 6
+
 # Get Dimensions
 
 def read_mesh(mesh_name):
@@ -39,3 +43,27 @@ def get_dimensions(path):
     for obj_name in obj_name_list:
         obj_dim_dict[obj_name] = get_obj_dimensions(obj_name)
     return obj_dim_dict
+
+# Point-cloud related
+
+def get_octree_size(node):
+    count = 0
+    if not isinstance(node, o3d.geometry.OctreeLeafNode):
+        for child in node.children:
+            if isinstance(child, o3d.geometry.OctreeNode):
+                childCount = get_octree_size(child)
+                count += childCount
+    else:
+        count = 1
+    return count
+
+def get_pcd_vol(pcd):
+
+    # get volumne of point cloud as estimate of voxels
+    voxel_grid = o3d.geometry.VoxelGrid.create_from_point_cloud(pcd, voxel_size=VOXEL_SIZE)
+    # voxels = voxel_grid.get_voxels()
+
+    octree = voxel_grid.to_octree(OCTREE_DEPTH)
+    octree_count = get_octree_size(octree.root_node)
+
+    return octree_count
